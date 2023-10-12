3e90614bb9413edfff583bb7d629d9f81fc116c1
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2023-10-12 18:29:27 +0600

    feat corsheaders

--- movies_backend/settings.py
+++ movies_backend/settings.py
@@ -69,6 +69,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'movies_backend.urls'
