0fab73b74f6b71fa01de3499d000e3c94cff7336
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2023-11-22 20:03:09 +0600

    feat commits and user fix list filters

--- movies_backend/settings.py
+++ movies_backend/settings.py
@@ -20,6 +20,12 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Указать путь к установленному ffmpeg
+ffmpeg_path = "/usr/bin/ffmpeg"
+
+# Установить переменную среды IMAGEIO_FFMPEG_EXE
+os.environ["IMAGEIO_FFMPEG_EXE"] = ffmpeg_path
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
